ce286c21eb004c02db906300b66a0887949ef408
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-04-11 15:18:10 +0530

    ad d d d

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,23 +4,21 @@ import { Link } from "gatsby"
 class Contact extends React.Component {
     render(){
         return(
-            <form 
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-        >
-            <div >
-
-                <input type="text" name="firstname"  placeholder="First Name"/>
-                <input type="text" name="lastname" placeholder="Last Name"/>
-                <input type="email" name="email"  placeholder="Email"/>
-                <input type="text" name="subject" placeholder="Subject"/>
-                <textarea name="message"  placeholder="Message" id="inputMessage" rows="3"></textarea>
-
-            <button type="submit" >Send Message</button>
-        </div>
-        </form>
+            <div>
+                <form 
+                    name="contact"
+                    method="POST"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                >
+                    <input type="text" name="firstname"  placeholder="First Name"/>
+                    <input type="text" name="lastname" placeholder="Last Name"/>
+                    <input type="email" name="email"  placeholder="Email"/>
+                    <input type="text" name="subject" placeholder="Subject"/>
+                    <textarea name="message"  placeholder="Message" id="inputMessage" rows="3"></textarea>
+                <button type="submit" >Send Message</button>
+                </form>
+            </div>
         )
     }
 }
