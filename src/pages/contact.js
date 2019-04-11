b7ea23d24480a5e5077e8175a3aec7f3f9b32197
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-04-11 15:25:21 +0530

    ad d d d

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,21 +4,29 @@ import { Link } from "gatsby"
 class Contact extends React.Component {
     render(){
         return(
-            <div>
-                <form 
-                    name="contact"
-                    method="POST"
-                    data-netlify="true"
-                    data-netlify-honeypot="bot-field"
-                >
-                    <input type="text" name="firstname"  placeholder="First Name"/>
-                    <input type="text" name="lastname" placeholder="Last Name"/>
-                    <input type="email" name="email"  placeholder="Email"/>
-                    <input type="text" name="subject" placeholder="Subject"/>
-                    <textarea name="message"  placeholder="Message" id="inputMessage" rows="3"></textarea>
-                    <button>Send Message</button>
-                </form>
-            </div>
+            // <div>
+            //     <form 
+            //         name="contact"
+            //         method="POST"
+            //         data-netlify="true"
+            //         data-netlify-honeypot="bot-field"
+            //     >
+            //         <input type="text" name="firstname"  placeholder="First Name"/>
+            //         <input type="text" name="lastname" placeholder="Last Name"/>
+            //         <input type="email" name="email"  placeholder="Email"/>
+            //         <input type="text" name="subject" placeholder="Subject"/>
+            //         <textarea name="message"  placeholder="Message" id="inputMessage" rows="3"></textarea>
+            //         <button>Send Message</button>
+            //     </form>
+            // </div>
+            <form
+    name="contact-form"
+    method="post"
+    data-netlify="true"
+  >
+    <input type="text" placeholder="name" name="name"/>
+    <button>Send</button>
+</form>
         )
     }
 }
