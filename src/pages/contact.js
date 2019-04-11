f145240d9bc50c7467ecfe85ef2ab222267677d8
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-04-11 16:37:26 +0530

    ADDDDDDDDDDDDD

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -25,7 +25,7 @@ class Contact extends React.Component {
             <Layout>
   <SEO title="Contact" />
   <Fade>
-        {/* <div>
+        <div>
             <form name=" contact"
                   method="post"
                   data-netlify="true"
@@ -38,41 +38,8 @@ class Contact extends React.Component {
              <input type="text" name="Subject" placeholder="subject" />
              <button>Save</button>
             </form>
-        </div> */}
-        <div>
-        <form 
-            name="contact"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            action="#"
-        >
-            <div className="form-row">
-                <div className="form-group col-lg-6">
-                <label htmlFor="firstName">First Name</label>
-                <input type="text" name="firstname" className="form-control" id="firstName" placeholder="First Name"/>
-                </div>
-
-                <div className="form-group col-lg-6">
-                <label htmlFor="inputLastName">Last Name</label>
-                <input type="text" name="lastname" className="form-control" id="inputLastName" placeholder="Last Name"/>
-                </div>
-            </div>
-            <div className="form-group">
-            <label htmlFor="inputEmail">Email</label>
-            <input type="email" name="email"  className="form-control" id="inputEmail" placeholder="Email"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="inputSubject">Subject</label>
-                <input type="text" name="subject" className="form-control" id="inputSubject" placeholder="Subject"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="inputMessage">Message </label>
-                <textarea name="message" className="form-control" placeholder="Message" id="inputMessage" rows="3"></textarea>
-            </div>
-        <button type="submit"  className="btn btn-primary">Send Message</button>
-        </form>
         </div>
+        
         </Fade>
 </Layout>
         )
