438238b0803d92b3833f5cfd63eddbef3fc9d216
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-04-11 16:10:18 +0530

    ADDDDDDDDDDDDD

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Fade from 'react-reveal/Fade';
 class Contact extends React.Component {
     render(){
         return(
@@ -18,7 +22,10 @@ class Contact extends React.Component {
             //         <button>Send Message</button>
             //     </form>
             // </div>
-        <div>
+            <Layout>
+  <SEO title="Contact" />
+  <Fade>
+        {/* <div>
             <form name=" contact"
                   method="post"
                   data-netlify="true"
@@ -31,7 +38,41 @@ class Contact extends React.Component {
              <input type="text" name="Subject" placeholder="subject" />
              <button>Save</button>
             </form>
-        </div>
+        </div> */}
+        <form 
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+        >
+            <div className="form-row">
+                <div className="form-group col-lg-6">
+                <label htmlFor="firstName">First Name</label>
+                <input type="text" name="firstname" className="form-control" id="firstName" placeholder="First Name"/>
+                </div>
+
+                <div className="form-group col-lg-6">
+                <label htmlFor="inputLastName">Last Name</label>
+                <input type="text" name="lastname" className="form-control" id="inputLastName" placeholder="Last Name"/>
+                </div>
+                
+            </div>
+            <div className="form-group">
+            <label htmlFor="inputEmail">Email</label>
+            <input type="email" name="email"  className="form-control" id="inputEmail" placeholder="Email"/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="inputSubject">Subject</label>
+                <input type="text" name="subject" className="form-control" id="inputSubject" placeholder="Subject"/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="inputMessage">Message </label>
+                <textarea name="message" className="form-control" placeholder="Message" id="inputMessage" rows="3"></textarea>
+            </div>
+        <button  className="btn btn-primary">Send Message</button>
+        </form>
+        </Fade>
+</Layout>
         )
     }
 }
